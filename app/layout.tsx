dadda97da1fbc764085fa408d9d492ee8caef784
escalacontrolORJ <productoslavictoriastd@gmail.com>
2026-01-09 10:01:20 -0500

    Fix layout syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,16 +17,12 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.className} bg-gray-50 text-black`}>
-        {/* El contenedor principal tiene un min-h-screen para asegurar que el fondo cubra todo.
-          El pb-24 (padding-bottom) es VITAL para que el contenido no quede oculto 
-          detrás del Navbar que está fijo abajo.
-        */}
+        {/* Contenedor con espacio inferior para el Navbar */}
         <div className="min-h-screen pb-24 relative">
           {children}
         </div>
         
-        {/* El Navbar se renderiza en todas las páginas, pero él mismo 
-            se oculta en el login '/' gracias a su lógica interna. */}
+        {/* Componente de navegación inferior */}
         <Navbar />
       </body>
     </html>
