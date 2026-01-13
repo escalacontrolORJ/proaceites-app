19162de6b03024276c7aef8af71b02fae8285e1c
escalacontrolORJ <productoslavictoriastd@gmail.com>
2026-01-13 12:42:14 -0500

    adminnav

--- app/admin/reportes/page.tsx
+++ app/admin/reportes/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-VERSION 2.0 - LIMPIEZA
+// VERSION 2.0 - LIMPIEZA
 import { useState, useEffect } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import AdminNav from '@/components/AdminNav'
